dccbd6998d249f96d2296d78715f3b907a3f5810
Reeta <retdream@gmail.com>
2017-09-15 16:15:04 +1000

    More routes

--- src/components/MoodApp.js
+++ src/components/MoodApp.js
@@ -6,8 +6,10 @@ function Nav (props) {
       <ul>
         <li>Home</li>
         <li>Overview</li>
+        <li onClick={() => {props.changeRoute('globalmood')}}>Global Mood</li>
         <li>Diary Entry</li>
         <li>Admin/Analytics</li>
+        <li onClick={() => {props.changeRoute('aboutus')}}>About Us</li>
         <li onClick={() => {props.changeRoute('signin')}}>SignIn</li>
         <li onClick={() => {props.changeRoute('signup')}}>SignUp</li>
       </ul>
@@ -15,7 +17,6 @@ function Nav (props) {
   );
 }
 
-<button onClick={() => { this.props.changeRoute('planes'); }}>Planes</button>
 
 function UserForm (props) {
   return (
@@ -26,6 +27,7 @@ function UserForm (props) {
   );
 }
 
+
 function SignIn (props) {
   return (
     <div className='signin'>
@@ -34,6 +36,7 @@ function SignIn (props) {
     </div>
   );
 }
+
 
 function SignUp (props) {
   return (
@@ -46,11 +49,34 @@ function SignUp (props) {
 }
 
 
+function AboutUs (props) {
+  return (
+    <div className='aboutus'>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  );
+}
+
+class GlobalMood extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className='globalmood'>
+        <p>Mood Globe Coming soon... in googlemaps or in a text globe</p>
+      </div>
+    );
+  }
+}
+
+
 export class MoodApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      route: 'signup'
+      route: 'aboutus'
     }
 
   this.changeRoute = this.changeRoute.bind(this);
@@ -69,6 +95,14 @@ export class MoodApp extends Component {
 
     if (routeName == 'signup') {
       content = <SignUp />;
+    }
+
+    if (routeName == 'aboutus') {
+      content = <AboutUs />;
+    }
+
+    if (routeName == 'globalmood') {
+      content = <GlobalMood />
     }
 
     return(
