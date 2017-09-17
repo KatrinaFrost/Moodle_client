90a186b903da75f9248434cc63bdb004d4beb9ca
Reeta <retdream@gmail.com>
2017-09-17 17:14:20 +1000

    Moods map in yearly calendar

---
+++ src/components/HeatMap.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Calendar, CalendarControls} from 'react-yearly-calendar'
+
+export function HeatMap(props) {
+  return ( <Calendar year = {2017} customClasses = {
+      (calendarDay) => {
+        let entriesToday = props.moodEntries.filter((moodEntry) => {
+          // .isSame is moment.js function that compares between two days or dates or months
+          return calendarDay.isSame(moodEntry.when, 'day');
+        });
+        return entriesToday.length ? 'mood-' + entriesToday[0].mood : '';
+      }
+    } />
+  )
+}
