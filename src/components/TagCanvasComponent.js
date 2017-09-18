15aedcaceccfec01c48a4e54e07bef50f08e6171
Reeta <retdream@gmail.com>
2017-09-19 09:33:08 +1000

    Tags adjusted

--- src/components/TagCanvasComponent.js
+++ src/components/TagCanvasComponent.js
@@ -5,7 +5,7 @@ import React, {Component} from 'react';
 export class TagCanvasComponent extends Component {
   componentDidMount() {
       try {
-        TagCanvas.Start('myCanvas','tags',{
+        TagCanvas.Start('myCanvas','tags', {
           textColour: '#ff0000',
           outlineColour: '#ff00ff',
           reverse: true,
@@ -17,45 +17,40 @@ export class TagCanvasComponent extends Component {
         document.getElementById('myCanvasContainer').style.display = 'none';
     };
   }
-render (){
-  return (
-    <div className='wrapper'>
-      <div id="myCanvasContainer">
-        <canvas width="300" height="300" id="myCanvas">
-          <p>Anything in here will be replaced on browsers that support the canvas element</p>
-          </canvas>
+  render (){
+    return (
+      <div className='wrapper'>
+        <div id="myCanvasContainer">
+          <canvas width="300" height="300" id="myCanvas">
+            <p>Anything in here will be replaced on browsers that support the canvas element</p>
+            </canvas>
+        </div>
+        <div id="tags">
+        <ul>
+          <li><a onClick={()=>{this.props.setMood(4)} } href="happy">Happy</a></li>
+          <li><a href="moods">Excited</a></li>
+          <li><a href="moods">Calm</a></li>
+          <li><a href="moods">Relaxed</a></li>
+          <li><a onClick={()=>{this.props.setMood(1)} } href="upset">Upset</a></li>
+          <li><a href="else">Bored</a></li>
+          <li><a href="else">Tensed</a></li>
+          <li><a href="else">Stressed</a></li>
+          <li><a href="else">Happy</a></li>
+          <li><a href="else">Excited</a></li>
+          <li><a href="else">Calm</a></li>
+          <li><a href="else">Relaxed</a></li>
+          <li><a href="else">Upset</a></li>
+          <li><a href="else">Bored</a></li>
+          <li><a href="else">Tensed</a></li>
+          <li><a href="else">Stressed</a></li>
+          <li><a href="else">Tensed</a></li>
+          <li><a href="else">Stressed</a></li>
+          <li><a href="else">Happy</a></li>
+          <li><a href="else">Excited</a></li>
+          <li><a href="else">Calm</a></li>
+        </ul>
       </div>
-      <div id="tags">
-      <ul>
-        <li><a href="somevalue">Mood</a></li>
-        <li><a href="somevalue">Hood</a></li>
-        <li><a href="somevalue">Table</a></li>
-        <li><a href="somevalue">Excited</a></li>
-        <li><a href="somevalue">Happy</a></li>
-        <li><a href="somevalue">Upset</a></li>
-        <li><a href="somevalue">Energetic</a></li>
-        <li><a href="somevalue">Amazing</a></li>
-        <li><a href="somevalue">Awesome</a></li>
-        <li><a href="somevalue">Mood</a></li>
-        <li><a href="somevalue">Hood</a></li>
-        <li><a href="somevalue">Table</a></li>
-        <li><a href="somevalue">Excited</a></li>
-        <li><a href="somevalue">Happy</a></li>
-        <li><a href="somevalue">Upset</a></li>
-        <li><a href="somevalue">Energetic</a></li>
-        <li><a href="somevalue">Amazing</a></li>
-        <li><a href="somevalue">Awesome</a></li>
-        <li><a href="somevalue">Mood</a></li>
-        <li><a href="somevalue">Hood</a></li>
-        <li><a href="somevalue">Table</a></li>
-        <li><a href="somevalue">Excited</a></li>
-        <li><a href="somevalue">Happy</a></li>
-        <li><a href="somevalue">Upset</a></li>
-        <li><a href="somevalue">Energetic</a></li>
-        <li><a href="somevalue">Amazing</a></li>
-        <li><a href="somevalue">Awesome</a></li>
-      </ul>
     </div>
-  </div>
-  );
-}}
+    );
+  }
+}
