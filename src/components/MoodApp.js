43109a22b0a9cbbe8f28a9facef0919e89a71da6
AmandaKitSam <amanda_sam@hotmail.co.uk>
2017-09-20 00:07:45 +1000

    MoodApp

--- src/components/MoodApp.js
+++ src/components/MoodApp.js
@@ -3,53 +3,10 @@ import axios from 'axios';
 import {BarChart} from './TestD3'
 import {WorldMap} from './WorldMap'
 import {HeatMap} from './HeatMap'
+import {TagCanvasComponent} from './TagCanvasComponent'
+import Diary from './Diary';
 
 const SERVER_PREFIX = 'http://localhost:2000/'
-
-const sampleMoodEntries = [{
-    when: new Date('2017-09-01'),
-    mood: 0
-  }, {
-    when: new Date('2017-09-02 10:03:00Z'),
-    mood: 2
-  }, {
-    when: new Date('2017-09-03'),
-    mood: 4
-  }, {
-    when: new Date('2017-09-04'),
-    mood: 1
-  }, {
-    when: new Date('2017-09-05'),
-    mood: 3
-  }, {
-    when: new Date('2017-09-06'),
-    mood: 0
-  }, {
-    when: new Date('2017-09-07'),
-    mood: 4
-  }, {
-    when: new Date('2017-09-08'),
-    mood: 2
-  }, {
-    when: new Date('2017-09-09'),
-    mood: 1
-  }, {
-    when: new Date('2017-09-10'),
-    mood: 4
-  }, {
-    when: new Date('2017-09-12'),
-    mood: 2
-  }, {
-    when: new Date('2017-09-13'),
-    mood: 3
-  }, {
-    when: new Date('2017-09-14'),
-    mood: 4
-  }, {
-    when: new Date('2017-09-16'),
-    mood: 0
-  }
-];
 
 class User extends Component {
   render() {
@@ -65,11 +22,12 @@ function MoodEntryForm(props) {
   return(
     <div className='mood_entry_form'>
       <h2>Hey <User name={props.name}/>! How is your mood today?</h2>
-      <img onClick={()=>{props.setMood(4)} } src='https://damonbraces.com/img/bethany-hamilton/emoji/DamonBraces_S016.png' />
-      <img onClick={()=>{props.setMood(3)} } src='https://cdn.shopify.com/s/files/1/0185/5092/products/persons-0025_large.png?v=1369543915' />
-      <img onClick={()=>{props.setMood(2)} } src='https://cdn.shopify.com/s/files/1/0185/5092/products/persons-0051_large.png?v=1369543585' />
-      <img onClick={()=>{props.setMood(1)} } src='https://4.bp.blogspot.com/-xjLbvdPaPBo/WJn4xvGuOmI/AAAAAAAAT9M/v8yMC3d7rnYcQQWIdQiizVMehoZmgY99ACLcB/s1600/tearful-emoji.png' />
-      <img onClick={()=>{props.setMood(0)} } src="https://t4.ftcdn.net/jpg/00/44/09/63/160_F_44096352_Owh22958YmSH8pPUXhX4RFlXXAKqESlT.jpg" />
+      <img alt='moods' onClick={()=>{props.setMood(4)} } src='https://damonbraces.com/img/bethany-hamilton/emoji/DamonBraces_S016.png' />
+      <img alt='moods' onClick={()=>{props.setMood(3)} } src='https://cdn.shopify.com/s/files/1/0185/5092/products/persons-0025_large.png?v=1369543915' />
+      <img alt='moods' onClick={()=>{props.setMood(2)} } src='https://cdn.shopify.com/s/files/1/0185/5092/products/persons-0051_large.png?v=1369543585' />
+      <img alt='moods' onClick={()=>{props.setMood(1)} } src='https://4.bp.blogspot.com/-xjLbvdPaPBo/WJn4xvGuOmI/AAAAAAAAT9M/v8yMC3d7rnYcQQWIdQiizVMehoZmgY99ACLcB/s1600/tearful-emoji.png' />
+      <img alt='moods' onClick={()=>{props.setMood(0)} } src="https://t4.ftcdn.net/jpg/00/44/09/63/160_F_44096352_Owh22958YmSH8pPUXhX4RFlXXAKqESlT.jpg" />
+      <p className='explain_user'>Record your daily mood by <strong>click</strong>ing on one of the emojis. You can change the record as many times as you like <strong>on the same day</strong>.</p>
     </div>
   );
 }
@@ -77,30 +35,10 @@ function MoodEntryForm(props) {
 function Footer (props) {
   return (
     <div className='footer'>
-      &#169; Amanda  ☁  Katrina  ⚒  Reeta ⚡
+      <span>&#169;</span> Amanda  ☁  Katrina  ⚒  Reeta ⚡
     </div>
   )
 }
-
-function Nav (props) {
-  return (
-    <div className='nav'>
-      <ul>
-        <li className='mood_logo' onClick={() => {props.changeRoute('home')}}>Inner Emoji</li>
-        <li><User name={props.name}/></li>
-        <li onClick={() => {props.changeRoute('overview')}}>Overview</li>
-        <li onClick={() => {props.changeRoute('globalmood')}}>Global Mood</li>
-        <li>Diary Entry</li>
-        <li onClick={() => {props.changeRoute('analytics')}}>Admin/Analytics</li>
-        <li onClick={() => {props.changeRoute('aboutus')}}>About Us</li>
-        <li onClick={() => {props.changeRoute('signin')}}>SignIn</li>
-        <li onClick={() => {props.changeRoute('signup')}}>SignUp</li>
-        <li onClick={() => {props.changeRoute('home')}}>Home</li>
-      </ul>
-    </div>
-  );
-}
-
 
 function UserForm (props) {
   return (
@@ -111,7 +49,6 @@ function UserForm (props) {
   );
 }
 
-
 function SignIn (props) {
   return (
     <div className='signin'>
@@ -120,7 +57,6 @@ function SignIn (props) {
     </div>
   );
 }
-
 
 function SignUp (props) {
   return (
@@ -132,7 +68,6 @@ function SignUp (props) {
   );
 }
 
-
 function AboutUs (props) {
   return (
     <div className='aboutus'>
@@ -141,7 +76,6 @@ function AboutUs (props) {
     </div>
   );
 }
-
 
 class GlobalMood extends Component {
   render() {
@@ -153,17 +87,34 @@ class GlobalMood extends Component {
   }
 }
 
+function Nav (props) {
+  return (
+    <div className='nav'>
+      <ul>
+        <li className='mood_logo' onClick={() => {props.changeRoute('home')}}>Inner Emoji</li>
+        <li><User name={props.name}/></li>
+        <li onClick={() => {props.changeRoute('overview')}}>Overview</li>
+        <li onClick={() => {props.changeRoute('globalmood')}}>Global Mood</li>
+        <li onClick={() => {props.changeRoute('diary')}}>Diary</li>
+        <li onClick={() => {props.changeRoute('analytics')}}>Admin/Analytics</li>
+        <li onClick={() => {props.changeRoute('aboutus')}}>About Us</li>
+        <li onClick={() => {props.changeRoute('signin')}}>SignIn</li>
+        <li onClick={() => {props.changeRoute('signup')}}>SignUp</li>
+        <li onClick={() => {props.changeRoute('home')}}>Home</li>
+      </ul>
+    </div>
+  );
+}
 
 export class MoodApp extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      route: 'home',
+      route: window.location.hash ? window.location.hash.slice(1) : 'home',
       users: [],
       current_user_id: null,
       moodEntries: []
     }
-
 
   this.changeRoute = this.changeRoute.bind(this);
   this.setMood = this.setMood.bind(this);
@@ -190,6 +141,7 @@ export class MoodApp extends Component {
   }
 
   changeRoute(route) {
+    window.location.hash = route;
     this.setState({route});
   }
 
@@ -197,10 +149,11 @@ export class MoodApp extends Component {
     axios.get(SERVER_PREFIX + 'user.json').then((results) => {
       this.setState({
         users: results.data,
-        current_user_id: results.data[0].id
+        current_user_id: results.data[2].id
       });
     });
   }
+
   getMoods() {
     axios.get(SERVER_PREFIX + 'mood_entry.json').then((results) => {
       this.setState({
@@ -212,6 +165,7 @@ export class MoodApp extends Component {
   componentDidMount(){
     this.getUsers();
     this.getMoods();
+
   }
 
   render() {
@@ -222,9 +176,14 @@ export class MoodApp extends Component {
     if (routeName === 'home') {
       content = <div className='wrapper'><MoodEntryForm name={userName} setMood={this.setMood}/><HeatMap moodEntries={this.state.moodEntries}/></div>;
     }
+    // <TagCanvasComponent />
 
     if (routeName === 'overview') {
       content = <div className='wrapper'><User name={userName}/><BarChart data={[5,6,7,2,4,7]} size={[500,500]}/></div>;
+    }
+
+    if (routeName === 'diary') {
+      content = <Diary />;
     }
 
     if (routeName === 'signin') {
@@ -238,8 +197,9 @@ export class MoodApp extends Component {
     if (routeName === 'aboutus') {
       content = (<AboutUs />);
     }
+
     if (routeName === 'analytics') {
-      content = (<BarChart data={[5,6,7,2,4,7]} size={[500,500]}/>);
+      content = (<BarChart data={[4,3,3,2,4,1]} size={[500,500]}/>);
     }
 
     if (routeName === 'globalmood') {
@@ -251,7 +211,6 @@ export class MoodApp extends Component {
         <Nav changeRoute={this.changeRoute} name={userName}/>
         {content}
         <Footer />
-
       </div>
     );
   }
